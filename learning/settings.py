2df064c4567dec8a97ec06a253c95e67fe6771cc
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-01-31 15:23:30 +0200

    5

--- learning/settings.py
+++ learning/settings.py
@@ -45,7 +45,12 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'learn.apps.LearnConfig',
     'newlearn.apps.NewlearnConfig',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
